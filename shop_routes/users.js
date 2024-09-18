2b27d04c4c5528b644dc8fc2dc9d3082eda6390b
AndreSosa21 <andrejulieth22@gmail.com>
2024-09-18 13:47:06 -0500

    endpoint_users

--- shop_routes/users.js
+++ shop_routes/users.js
@@ -5,6 +5,19 @@ const route_users = express.Router();
 //using required router
 
 let users = [
+    {
+        id: "2",
+        name: "Andre",
+        email: "Andre@gmail.com",
+        age: "19"
+    },
+    {
+        
+        id: "3",
+        name: "samu",
+        email: "samu@gmail.com",
+        age: "18"
+    }
 
 ]
 //GET all users
@@ -12,41 +25,56 @@ route_users.get('/' , (req, res) =>{
     res.send(users);
 })
 
-//GET user by id
+//GET user by id //VALIDATIONS
 route_users.get('/:id' , (req, res) =>{
     const id=req.params.id;
     const user = users.find(user => user.id === id); 
     if(!user){
         
-        return res.status(404).send('User found');
+        return res.status(404).send('User not found');
     }
     res.json(user);
 
 })
 
-//POST users
+//POST users //VALIDATIONS
 route_users.post('/' , (req, res) =>{
-    const new_user = req.body;
-    
-    users.push(new_user);
-    res.send(users);
-})
+    const {id,name, email , age} = req.body;
+    //incomplete JSON
+    try{
+        if (!id || !name || !email || !age) {
+            return res.status(400).json({ error: 'json incomplete' });
+        }
+        const new_user = {id, name, email, age};
+        users.push(new_user);
+        res.send(users);
 
-//PUT users by id
-route_users.put('/:id' , (req, res) =>{
-    const update_user = req.body;
-    const id = req.params.id;
-    const index = users.findIndex( i => i.id == id);
-
-    if(index < 0){
-        return res.status(404).send('User not found');
+    }catch(error){
+        res.status(500).json({message:"user not added", error:error.message});
     }
-    
-    users[index] = update_user;
-    res.send(users);
-    
 })
-//DELETE user
+
+//PUT users by id //VALIDATIONS
+route_users.put('/:id' , (req, res) =>{
+    const {name, email , age} = req.body;
+    try{
+        const id = req.params.id;
+        const index = users.findIndex( i => i.id == id);
+        if(index < 0 ){
+            return res.status(404).send('Id user does not exist');
+        }
+        else if(!id || !name || !email || !age){
+            return res.status(400).json({ error: 'json incomplete' }); 
+        }
+        const update_user={id,name,email,age};
+        users[index] = update_user;
+        res.send(users);
+    }
+    catch(error){
+        res.status(500).json({message:"user not founded", error:error.message});
+    }  
+})
+//DELETE user //VALIDATIONS
 route_users.delete('/:id' , (req, res) =>{
     
     const id = req.params.id;
@@ -60,9 +88,6 @@ route_users.delete('/:id' , (req, res) =>{
     res.send(users);
     
 })
-
-
-
 module.exports= route_users;
 
 
